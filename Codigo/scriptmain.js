2e620401e0144e6943bfad46eec15b597cda84ba
João Pedro Mairinque <jpmairinque@gmail.com>
2021-05-12 11:04:17 -0300

    c5 fetch api

--- Codigo/scriptmain.js
+++ Codigo/scriptmain.js
@@ -2,42 +2,28 @@ console.log("akwlqdew");
 
 const cep = document.querySelector("#cep");
 
-
-
-
-
-cep.addEventListener("blur",()=> {
-
-
-    
-    const monteCep = (result) =>{
-        for (const campo in result){
-            if(document.querySelector("#" + campo)){
-                let space = document.querySelector("#" + campo)
-                space.value= result[campo]
-            }
-        }
+cep.addEventListener("blur", () => {
+  const monteCep = (result) => {
+    for (const campo in result) {
+      if (document.querySelector("#" + campo)) {
+        let space = document.querySelector("#" + campo);
+        space.value = result[campo];
+      }
     }
+  };
 
+  let pesquisa = cep.value.replace("-", "");
+  const opt = {
+    method: "GET",
+    mode: "cors",
+    cache: "default",
+  };
 
-
-    
-    let pesquisa = cep.value.replace("-","")
-    const opt = {
-        method: 'GET',
-        mode: 'cors',
-        cache: 'default'
-    }
-
-    fetch(`https://viacep.com.br/ws/${pesquisa}/json/`, opt)
-    .then((res)=>{res.json()
-        .then((data) => monteCep(data))
+  fetch(`https://viacep.com.br/ws/${pesquisa}/json/`, opt)
+    .then((res) => {
+      res.json().then((data) => monteCep(data));
     })
-    .catch(e => console.log('Deu Erro: ' + e,message))
-
-    
-    
-  })
-
+    .catch((e) => console.log("Deu Erro: " + e, message));
+});
 
 //fetch()  https://viacep.com.br/ws/01001000/json/
