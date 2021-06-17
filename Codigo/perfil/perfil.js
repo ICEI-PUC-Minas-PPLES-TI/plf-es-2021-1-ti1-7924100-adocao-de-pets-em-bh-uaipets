b416d3791495efa572b59e284f636cfc4d213489
João Pedro Mairinque <jpmairinque@gmail.com>
2021-06-17 20:10:03 -0300

    profile

--- Codigo/perfil/perfil.js
+++ Codigo/perfil/perfil.js
@@ -1,16 +1,40 @@
+
+
+
 const changeProfile = () => {
 
-   
-    var nameinput = document.getElementById("nameInput")
     
-    nameinput.innerHTML = `<input type="text" id="namevalue" placeholder="insira seu nome"> 
-    <button onclick="changeName()"> ok </button>`
 
-    function changeName(){
+    var nameinput = document.getElementById("nameInput")
+    var emailinput = document.getElementById("emailInput")
+    var phoneinput = document.getElementById("phoneInput")
+   var  enderecoinput = document.getElementById("addressInput")
 
-        console.log(document.getElementById("namevalue").value)
+    
+    nameinput.innerHTML = `<input type="text" id="namevalue" value="a"  placeholder="digite seu nome"> `
+   emailinput.innerHTML = `<input type="text" id="emailvalue"  placeholder="digite seu email"> `
+    phoneinput.innerHTML = `<input type="text" id="phonevalue"  placeholder="digite seu telefone"> `
+    enderecoinput.innerHTML = `<input type="text" id="enderecovalue"  placeholder="digite seu endereço"> `
 
+
+   
+
+    
+    
+
+    var but = document.getElementById("changesave")
+    but.innerHTML=`Salvar`
+    but.addEventListener('click', ()=>{
+
+        console.log(nameinput)
+      
+        // nameinput.innerHTML=`${namevalue}`
+
+    })
+
+
+   function changeName(){
+        console.log('a')
     }
-}
 
-changeProfile() 
+}
