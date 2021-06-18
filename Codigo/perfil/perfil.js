151c088f775b3b034832925b95adf58ba4ae2497
Ana Luiza <analuizaalvesm@gmail.com>
2021-06-17 23:16:27 -0300

    perfil

--- Codigo/perfil/perfil.js
+++ Codigo/perfil/perfil.js
@@ -1,40 +1,58 @@
 
+onload = () => {
+    if (localStorage.getItem("user") === null)
+        setData('Alberto Silva', 'albertosilva@email.com', '(31) 91234-5678', 'Rua Sapucaí, 33 - Floresta', 'Estudante de veterinária e amante de gatos nas horas vagas.');
+    updateProfile();
+}
 
+const changeProfile = () =>{
+    var nameInput = document.getElementById("nameValue");
+    var emailInput = document.getElementById("emailValue");
+    var phoneInput = document.getElementById("phoneValue");
+    var enderecoInput = document.getElementById("addressValue");
+    var aboutInput = document.getElementById("aboutValue");
+    var dataObj = getData(); 
 
-const changeProfile = () => {
+    nameInput.value = dataObj.name;
+    emailInput.value = dataObj.email;
+    phoneInput.value = dataObj.phone;
+    enderecoInput.value = dataObj.address;
+    aboutInput.value = dataObj.about;
+    $('.info-text').hide();
+    $('.info-input').show();
+}
 
-    
+function saveProfile(){
+    var nameInput = document.getElementById("nameValue").value;
+    var emailInput = document.getElementById("emailValue").value;
+    var phoneInput = document.getElementById("phoneValue").value;
+    var enderecoInput = document.getElementById("addressValue").value;
+    var aboutInput = document.getElementById("aboutValue").value;
 
-    var nameinput = document.getElementById("nameInput")
-    var emailinput = document.getElementById("emailInput")
-    var phoneinput = document.getElementById("phoneInput")
-   var  enderecoinput = document.getElementById("addressInput")
+    setData(nameInput, emailInput, phoneInput, enderecoInput, aboutInput);
+    $('.info-text').show();
+    $('.info-input').hide();
+    updateProfile();
+}
 
-    
-    nameinput.innerHTML = `<input type="text" id="namevalue" value="a"  placeholder="digite seu nome"> `
-   emailinput.innerHTML = `<input type="text" id="emailvalue"  placeholder="digite seu email"> `
-    phoneinput.innerHTML = `<input type="text" id="phonevalue"  placeholder="digite seu telefone"> `
-    enderecoinput.innerHTML = `<input type="text" id="enderecovalue"  placeholder="digite seu endereço"> `
+function updateProfile(){
+    var dataObj = getData();
+    var allName = document.querySelectorAll('.name-text');
+    allName.forEach(name => {
+        name.innerHTML = dataObj.name;
+    });
+    document.getElementById("email").innerHTML = dataObj.email;
+    document.getElementById("phone").innerHTML = dataObj.phone;
+    document.getElementById("address").innerHTML = dataObj.address;
+    document.getElementById("about").innerHTML = dataObj.about;
+}
 
+function setData(name, email, phone, address, about = ''){
+    localStorage.setItem('user', JSON.stringify({ name: name, email: email, phone: phone, address: address, about: about,}));
 
-   
+}
 
-    
-    
-
-    var but = document.getElementById("changesave")
-    but.innerHTML=`Salvar`
-    but.addEventListener('click', ()=>{
-
-        console.log(nameinput)
-      
-        // nameinput.innerHTML=`${namevalue}`
-
-    })
-
-
-   function changeName(){
-        console.log('a')
-    }
-
+function getData(){
+    console.log(JSON.parse(localStorage.getItem('user')));
+    return JSON.parse(localStorage.getItem('user'));
 }
